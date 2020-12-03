e3336d151f34b111eaf70c301560698569350b80
owlxx <50915025+owlnxx@users.noreply.github.com>
2020-12-03 15:40:03 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const txtElement = ['Front End Developer.', 'Highschool Student.', 'Video Editor.']
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik() {
+
+    if (count == txtElement.length) {
+        count = 0;
+    }
+
+    currentTxt = txtElement[count];
+
+    words = currentTxt.slice(0, ++txtIndex)
+    console.log(words);
+    document.querySelector('.typing-effect').textContent = words;
+
+    if (words.length == currentTxt.length) {
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(ngetik, 130);
+
+})();
